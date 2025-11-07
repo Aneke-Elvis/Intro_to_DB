2694a22a675c2b0d32dac5289e0e58f3badc6213
Aneke-Elvis <anekeelvis23@gmail.com>
2025-11-07 19:36:06 +0100

    Fixing Order table checker issues

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -17,7 +17,7 @@ CREATE TABLE Books (
   author_id INT NOT NULL,
   price DOUBLE NOT NULL,
   publication_date DATE,
-  FOREIGN KEY (author_id) REFERENCES authors(author_id)
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 ) ;
 
 -- Customers table
@@ -33,7 +33,7 @@ CREATE TABLE Orders (
   order_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT NOT NULL ,
   order_date DATE NOT NULL,
-  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 ) ;
 
 -- Order_Details table
@@ -42,6 +42,6 @@ CREATE TABLE Order_details (
   order_id INT NOT NULL,
   book_id INT NOT NULL,
   quantity DOUBLE NOT NULL,
-  FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  FOREIGN KEY (book_id) REFERENCES books(book_id)
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
 ) ;
