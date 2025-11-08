e99dc9d9625315eb6c672851fb34967663bc5c2d
Aneke-Elvis <anekeelvis23@gmail.com>
2025-11-09 00:10:37 +0100

    fix connection error handling by checker standard

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,7 @@ Creates a database named alx_book_store if it doesn't already exist.
 """
 
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import Error, errorcode
 
 def create_database():
     connection = None
@@ -14,16 +14,19 @@ def create_database():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",        # <-- change if your MySQL username differs
-            password="Chiboy2025"  # <-- replace with your MySQL password
+            password="xxx"  # <-- replace with your MySQL password
         )
 
         if connection.is_connected():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
             print("Database 'alx_book_store' created successfully!")
-
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+    except mysql.connector.Error as err:
+        # 3. Handle connection and execution errors
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Error: Access denied. Check your username and password.")
+        else:
+            print(f"Failed to connect to the database: {err}")
 
     finally:
         if connection is not None and connection.is_connected():
