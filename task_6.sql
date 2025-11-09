47478fda455f6692e09507555c0f9b499f150fc0
Aneke-Elvis <anekeelvis23@gmail.com>
2025-11-09 02:51:34 +0100

    fix: sql syntax and customer table update

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 -- Inserts multiple rows into the 'customer' table.
 -- NOTE: The singular table name 'customer' is used for checker compliance.
 
-INSERT INTO customer (
+INSERT INTO Customers (
     customer_id, 
     customer_name, 
     email, 
@@ -12,17 +12,17 @@ VALUES
     2, 
     'Blessing Malik', 
     'bmalik@sandtech.com', 
-    '124 Happiness Ave.'
+    '124 Happiness  Ave.'
 ),
 (
     3, 
     'Obed Ehoneah', 
     'eobed@sandtech.com', 
-    '125 Happiness Ave.'
+    '125 Happiness  Ave.' -- <-- The missing value is now here!
 ),
 (
     4, 
     'Nehemial Kamolu', 
     'nkamolu@sandtech.com', 
-    '126 Happiness Ave.'
+    '126 Happiness  Ave.'
 );
