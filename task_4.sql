f232d2f50eea179e8426c06f2bb42b073b472423
Aneke-Elvis <anekeelvis23@gmail.com>
2025-11-09 01:54:11 +0100

    add sql script to query the book table

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- Query the Information Schema to get the full description of the 'books' table.
+-- The database name is assumed to be passed as an argument to the MySQL command.
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = DATABASE() 
+AND 
+    TABLE_NAME = 'Books';
